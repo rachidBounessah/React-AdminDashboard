c638a31e70ba8ddcbac5de92a65f9ae4bb7794b4
rachid <rachid12br@gmail.com>
2024-06-27 12:03:24 -0400

    add search component and stack with (icons) and box (for spacing with flexGrow:)

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,10 @@
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton, Toolbar, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Toolbar } from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
-import { styled, useTheme } from "@mui/material/styles";
-import React from "react";
-
+import { styled, alpha } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 const drawerWidth = 240;
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
@@ -24,6 +25,45 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
 export default function TopBar({ open, handleDrawerOpen }) {
   return (
     <AppBar
@@ -44,9 +84,31 @@ export default function TopBar({ open, handleDrawerOpen }) {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
-          Mini variant drawer
-        </Typography>
+
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+        <Box flexGrow={1} />
+        <Stack direction={"row"}>
+          <IconButton color="inherit" aria-label="add to shopping cart">
+            <AddShoppingCartIcon />
+          </IconButton>
+          <IconButton color="inherit" aria-label="add to shopping cart">
+            <AddShoppingCartIcon />
+          </IconButton>
+          <IconButton color="inherit" aria-label="add to shopping cart">
+            <AddShoppingCartIcon />
+          </IconButton>
+          <IconButton color="inherit" aria-label="add to shopping cart">
+            <AddShoppingCartIcon />
+          </IconButton>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
