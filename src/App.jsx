f9ff085ce680acfc8820683b2ef098f727e3d645
rachid <rachid12br@gmail.com>
2024-06-27 11:15:24 -0400

    SideBar.jsx  TopBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-function App() {
-  return <div>hello rachid its you'r day today</div>;
-}
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 
-export default App;
+import CssBaseline from "@mui/material/CssBaseline";
+
+import TopBar from "./components/TopBar";
+import SideBar from "./components/SideBar";
+
+export default function MiniDrawer() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <TopBar open={open} handleDrawerOpen={handleDrawerOpen} />
+      <SideBar open={open} handleDrawerClose={handleDrawerClose} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}></Box>
+    </Box>
+  );
+}
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,53 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import { IconButton, Toolbar, Typography } from "@mui/material";
+import MuiAppBar from "@mui/material/AppBar";
+import { styled, useTheme } from "@mui/material/styles";
+import React from "react";
+
+const drawerWidth = 240;
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+  // @ts-ignore
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export default function TopBar({ open, handleDrawerOpen }) {
+  return (
+    <AppBar
+      position="fixed"
+      // @ts-ignore
+      open={open}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{
+            marginRight: 5,
+            ...(open && { display: "none" }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Mini variant drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
